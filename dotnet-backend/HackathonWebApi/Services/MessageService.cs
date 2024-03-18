828cee650164bd93eded36ab287df391867bf755
Christopher Welzl <christopher.welzl@usz.ch>
2024-03-18 16:36:43 +0100

    add json schema

--- dotnet-backend/HackathonWebApi/Services/MessageService.cs
+++ dotnet-backend/HackathonWebApi/Services/MessageService.cs
@@ -5,13 +5,18 @@ namespace HackathonWebApi.Services
 {
     public class MessageService
     {
-        private static readonly string BaseQuestion = $"Answer in less than {Config.MaxTokens}. Can you give me a one month workout plan based on the following input: ";
+        private static readonly string BaseQuestion = $"Answer in less than {Config.MaxTokens}. Respond in a Json with the following Schema: {GetJsonSchema()} Can you give me a one month workout plan based on the following input: ";
         public static string GetConstructedMessage(UserInput input)
         {
             StringBuilder stringBuilder = new StringBuilder();
             stringBuilder.Append(BaseQuestion).Append(input.FreeText);
             return stringBuilder.ToString();
+        }
 
+        private static string GetJsonSchema()
+        {
+            return File.ReadAllText(
+                "C:\\Dev\\Hackathon2024\\hackathon\\dotnet-backend\\HackathonWebApi\\Json\\WorkoutPlan.json");
         }
     }
 }
